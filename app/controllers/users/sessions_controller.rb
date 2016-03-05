7aa1a680ec5374c23203d240f4d6a3946dfac39e
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2016-03-05 21:58:37 +0800

    Configure Tiddle and delete unused devise controllers

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,25 +1,20 @@
 class Users::SessionsController < Devise::SessionsController
-# before_filter :configure_sign_in_params, only: [:create]
+  
+  def create
+    user  = warden.authenticate!(auth_options)
+    token = Tiddle.create_and_return_token(user, request)
+    render json: { authentication_token: token }
+  end
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def destroy
+    Tiddle.expire_token(current_user, request) if current_user
+    render json: {}
+  end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  private
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+    # this is invoked before destroy and we have to override it
+    def verify_signed_out_user
+    end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
-  # end
 end
